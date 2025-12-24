547ef11f97f5c5f24f58cd7450b5a89e227e8705
Zakiya <zakiyamanzoor04@gmail.com>
2025-12-24 11:23:21 +0530

    add pg to project

--- basics/index.js
+++ basics/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import Users from "./users.json" assert { type: 'json' };
-import fs from "fs";
+import userRouter from "./routes/user.route.js"
+import logReqRes from "./middlewares/index.middle.js";
 
 const app = express();
 const PORT = 8010;
@@ -9,67 +9,18 @@ const PORT = 8010;
 //middleware
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req,res,next)=>{
-    fs.appendFile('log.txt',`${Date.now()}:${req.method}: ${req.path}`,(err)=>{
-        if(err){
-            console.log(err)
-        }
-        next()
-    })
-})
+app.use(logReqRes("log.txt"))
 
 //Hybrid server for mobile part
-app.get("/users",(req,res)=>{
+app.get("/users",async (req,res)=>{
+    const allUsers = await pool.query(`SELECT * FROM "user";`);
     const html =`<ul>
-      ${Users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+      ${allUsers.rows.map((user)=>`<li>${user.first_name}-${user.email}</li>`).join("")}
     </ul>`
     res.send(html)
 })
 
 //Rest APIs
-app.get("/api/users",(req,res)=>{
-    return res.json(Users)
-});
-
-app.route("/api/users/:id").get((req,res)=>{
-    const id = Number(req.params.id);
-    const user = Users.find(user => user.id === id);
-    return res.json(user);
-}).patch((req,res)=>{
-    const id = Number(req.params.id);
-    const user = Users.find(user => user.id === id);
-
-    Object.assign(user,req.body);
-
-    fs.writeFile("./users.json",JSON.stringify(Users),(err)=>{
-        if (err){
-            console.log(err)
-        }
-        return res.json({status:"Updated!!"})
-    })
-}).delete((req,res)=>{
-   const id = Number(req.params.id);
-   const index = Users.findIndex(user => user.id === id);
-
-   if(index !== -1){
-    Users.splice(index,1)
-   }
-  
-   fs.writeFile("./users.json", JSON.stringify(Users),(err)=>{
-    if(err){
-        console.log(err)
-    }
-    return res.json({status:"Success!!"})
-   })
-});
-
-app.post('/api/users',(req,res)=>{
-    const data = req.body;
-    Users.push({id:Users.length + 1 , ...data});
-    fs.writeFile("./users.json",JSON.stringify(Users),(err,data)=>{
-        return res.json({status:"Success!!"})
-    })
-});
-
+app.use("/api/users", userRouter)
 
 app.listen(PORT,()=> console.log("Server is running!"))
