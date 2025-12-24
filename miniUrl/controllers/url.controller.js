e1dbd3941b3f50c7919a41ebd93fe06f56beb104
Zakiya <zakiyamanzoor04@gmail.com>
2025-12-24 21:53:54 +0530

    add routes handler

---
+++ miniUrl/controllers/url.controller.js
@@ -0,0 +1,69 @@
+import { createUrl, getUrl, getClicks, getAllUrls} from "../models/url.model.js";
+import {nanoid} from "nanoid";
+
+export async function handleCreateUrl(req,res){
+    const {url} = req.body;
+    const shortUrl = nanoid(8);
+
+    if(!url){
+      return  res.status(400).json({message:"URL not provided!"})
+    }
+
+    try{
+    const newUrl = await createUrl(url,shortUrl);
+    return res.status(201).json({Created: newUrl})
+    }
+    catch(err){
+      console.log(err)
+     return res.status(500).json({message:"Could'nt Create URL!"})
+    }
+};
+
+export async function handleGetUrl(req,res){
+   const url = req.params.shorturl;
+
+    if(!url){
+    return res.status(400).json({message:"Enter your url"});
+   }
+
+   try{
+    const realUrl = await getUrl(url);
+    return res.redirect(realUrl.redirect_url)
+  }
+   catch(err){
+    console.log(err);
+    res.status(500).json({message:"Could not get the URL"})
+  }
+   
+};
+
+export async function handleAnalytics(req,res){
+  const url = req.params.shorturl;
+  if(!url){
+   return res.status(400).json({message:"Insert Url"})
+  }
+  try{ 
+    const number = await getClicks(url);
+    return res.status(200).json({totalClicks:number})
+  }
+  catch(err){
+    console.log(err);
+    return res.status(500).json({message:"Couldn't Get the user"})
+  }
+};
+
+export async function handleGetUrls(req,res){
+
+  try{
+    const urls = await getAllUrls();
+
+    if(!urls){
+    return res.status(400).json({message:"Couldnt get urls"})
+    }
+    return res.render('home')
+  }
+  catch(err){
+    console.log(err);
+   return res.status(500).json({message:"Couldn't Get the urls"})
+  }
+}
