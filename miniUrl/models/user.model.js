b8ee7e81cea4e72f4fdcb770db9c8a421fbaad61
Zakiya <zakiyamanzoor04@gmail.com>
2025-12-27 22:14:00 +0530

    add user to db

--- miniUrl/models/user.model.js
+++ miniUrl/models/user.model.js
@@ -1,6 +1,11 @@
 import pool from "../connection.js";
 
 export async function signUp(name,email,password){
-  const result = await pool.query(`INSERT INTO "user" (name,email,passsword) VALUES ($1, $2, $3) RETURNING *`,[name,email,password]);
+  const result = await pool.query(`INSERT INTO "user" (first_name,email,password) VALUES ($1, $2, $3) RETURNING *`,[name,email,password]);
   return result.rows[0];
-}
+};
+
+export async function logIn(email,password){
+  const result = await pool.query(`SELECT * FROM "user" WHERE email = $1 AND password = $2`,[email,password]);
+  return result.rows[0];
+};
