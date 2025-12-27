8b9e8b360c65bdc3f8077321213ca73dda45e508
Zakiya <zakiyamanzoor04@gmail.com>
2025-12-27 22:12:30 +0530

    add login function

--- miniUrl/routes/user.route.js
+++ miniUrl/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { handleSignUp } from '../controllers/user.controller.js';
+import { handleSignUp, handleLogIn} from '../controllers/user.controller.js';
 
-export const userRoute = express.Router();
+const userRoute = express.Router();
 
 userRoute.post("/", handleSignUp);
+userRoute.post("/login",handleLogIn)
 
 export default userRoute;
