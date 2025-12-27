fce0cf3bbd08dbc70078c1f292389a9ab704db60
Zakiya <zakiyamanzoor04@gmail.com>
2025-12-27 22:11:38 +0530

    add ejs for home page

--- miniUrl/routes/home.route.js
+++ miniUrl/routes/home.route.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import { handleGetUrls } from '../controllers/url.controller.js';
 
-export const homeRouter = express.Router();
+const homeRouter = express.Router();
+
+homeRouter.get('/',(req,res)=>{
+    if(!req.body) return res.redirect('/login');
+
+ handleGetUrls(req,res)
+});
 
 
-homeRouter.get('/',handleGetUrls)
+homeRouter.get('/signup',(req,res)=>{
+    return res.render('signup')
+});
+homeRouter.get('/login',(req,res)=>{
+    return res.render('login')
+})
 
 export default homeRouter;
