b24b07e9756354b097bc392722528acd6f431ee1
Zakiya <zakiyamanzoor04@gmail.com>
2026-01-01 14:15:21 +0530

    initial commit

---
+++ discord-bot/commands.js
@@ -0,0 +1,21 @@
+import { REST, Routes } from 'discord.js';
+import 'dotenv/config';
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands("1456102130363535585"), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
