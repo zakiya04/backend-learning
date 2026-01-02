3352d916cdfee43d516d641066919e05d291c415
Zakiya <zakiyamanzoor04@gmail.com>
2026-01-02 12:13:12 +0530

    add command function

--- discord-bot/commands.js
+++ discord-bot/commands.js
@@ -6,6 +6,18 @@ const commands = [
     name: 'ping',
     description: 'Replies with Pong!',
   },
+  {
+    name: 'create',
+    description: 'Makes a url shortener for a given url',
+     options: [
+      {
+        name: 'url',
+        description: 'The URL to shorten',
+        type: 3, 
+        required: true
+      }
+    ]
+  }
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
