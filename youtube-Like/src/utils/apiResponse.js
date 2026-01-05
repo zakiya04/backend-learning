9434a93ef2c40faa298e7292a6b11f2b94c97d6c
Zakiya <zakiyamanzoor04@gmail.com>
2026-01-05 21:52:59 +0530

    blueprint for response

---
+++ youtube-Like/src/utils/apiResponse.js
@@ -0,0 +1,8 @@
+export class ApiResponse {
+    constructor(statusCode, message="Success", data, ){
+        this.statusCode = statusCode
+        this.message = message
+        this.data = data
+        this.success = statusCode < 300
+    }
+}
