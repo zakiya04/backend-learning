c684ded77ea7b6dc0962ea003adb588268a7ff41
Zakiya <zakiyamanzoor04@gmail.com>
2026-01-07 22:33:45 +0530

    some problem with multer

--- youtube-Like/src/utils/cloudinary.js
+++ youtube-Like/src/utils/cloudinary.js
@@ -3,13 +3,14 @@ import fs from 'fs'
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME, 
-    api_key:  process.env.CLOUDINARY_API_KEY, 
-    api_secret:  process.env.CLOUDINARY_API_SECRET
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export const uploadOnCloudinary = async (localFilePath)=>{
+
  try {
-    if(!localFilePath) return console.log("Could'nt the file for images!");
+    if(!localFilePath) return console.log("Could'nt get the file for images!");
 
     const response = await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"});
     console.log("file uploaded to cloudiary", response.url);
