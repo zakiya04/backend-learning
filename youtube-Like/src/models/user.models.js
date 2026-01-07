30dc53c06f4cdf9d0179dc8b08c9bb42c1b82c27
Zakiya <zakiyamanzoor04@gmail.com>
2026-01-07 22:34:50 +0530

    complete user schema

--- youtube-Like/src/models/user.models.js
+++ youtube-Like/src/models/user.models.js
@@ -51,7 +51,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function(next){
   if(!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash('password',10);
+  this.password = await bcrypt.hash('password',10);
   next()
 });
 
