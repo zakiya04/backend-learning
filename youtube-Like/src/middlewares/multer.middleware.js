eadcb00e4356a007f74d8b76d312fc972504e119
Zakiya <zakiyamanzoor04@gmail.com>
2026-01-07 22:31:54 +0530

    couldnt get what is happening

--- youtube-Like/src/middlewares/multer.middleware.js
+++ youtube-Like/src/middlewares/multer.middleware.js
@@ -2,11 +2,11 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/tmp/my-uploads')
+   cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
   }
 })
 
-const upload = multer({ storage })
+export const upload = multer({ storage })
