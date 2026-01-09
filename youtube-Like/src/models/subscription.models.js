41877065090fbd676d63ac0e7637a6c4c476e933
Zakiya <zakiyamanzoor04@gmail.com>
2026-01-09 11:38:07 +0530

    fix a typo

--- youtube-Like/src/models/subscription.models.js
+++ youtube-Like/src/models/subscription.models.js
@@ -1,7 +1,7 @@
 import {mongoose, Schema} from "mongoose"
 
 const subscriptionSchema = new Schema({
-    creator:{
+    subscriber:{
         type: Schema.Types.ObjectId,
         ref:"User"
     },
